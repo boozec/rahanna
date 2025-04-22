71520594ca2ae593b64039a765a9d908565968c8
Santo Cariotti <santo@dcariotti.me>
2025-04-22 10:18:06 +0200

    release: version 0.0.9-2

--- release.sh
+++ release.sh
@@ -9,4 +9,4 @@ sed -i "15s/.*/LABEL version=\"$1\"/" docker/api/Dockerfile
 sed -i "17s/.*/LABEL version=\"$1\"/" docker/ui/Dockerfile
 git add docker
 git commit -m "release: version $1"
-git tag -a "v$1" -m "Version $1"
+git tag -a "$1" -m "Version $1"
