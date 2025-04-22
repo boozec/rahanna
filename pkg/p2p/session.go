dd356e1a7027442b5bced97fa3185e7e143cc7e5
Santo Cariotti <santo@dcariotti.me>
2025-04-22 09:06:19 +0200

    More session names

--- pkg/p2p/session.go
+++ pkg/p2p/session.go
@@ -6,14 +6,28 @@ import (
 
 var adjectives = []string{
 	"adamant", "adept", "adventurous", "arcadian", "auspicious",
-	"awesome", "blossoming", "brave", "charming", "chatty",
-	"circular", "considerate", "cubic", "curious", "delighted",
+	"awesome", "blossoming", "bold", "brave", "bubbly",
+	"charming", "chatty", "circular", "colorful", "considerate",
+	"cosmic", "cuddly", "curious", "dazzling", "delighted",
+	"dynamic", "eager", "ecstatic", "effervescent", "elated",
+	"elegant", "elvish", "fanciful", "fearless", "festive",
+	"fluffy", "forceful", "glorious", "goofy", "graceful",
+	"gutsy", "happy", "hobbity", "jedi", "mystical",
+	"potteresque", "rebel", "slytherin", "valiant", "witty",
 }
 
 var nouns = []string{
-	"aardvark", "accordion", "apple", "apricot", "bee",
-	"brachiosaur", "cactus", "capsicum", "clarinet", "cowbell",
-	"crab", "cuckoo", "cymbal", "diplodocus", "donkey",
+	"aardvark", "accordion", "apple", "apricot", "asteroid",
+	"bantha", "bee", "bison", "brachiosaur", "bubble",
+	"cactus", "capsicum", "clarinet", "cloud", "cowbell",
+	"crab", "cuckoo", "cupcake", "cymbal", "diplodocus",
+	"donkey", "dragon", "drum", "dobby", "elf",
+	"eel", "emu", "ent", "falcon", "fern",
+	"flamingo", "giraffe", "glacier", "goose", "grapefruit",
+	"gryffindor", "hamster", "harmonica", "hedgehog", "honeybee",
+	"hufflepuff", "hummingbird", "lightsaber", "mandalorian", "mushroom",
+	"orchestra", "orcrist", "phoenix", "ringwraith", "snitch",
+	"stormtrooper", "tatooine", "wand", "wookiee", "yoda",
 }
 
 func NewSession() string {
