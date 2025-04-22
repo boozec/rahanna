ba2937f2680c2bfd5054c162e390729d0a7d3191
Santo Cariotti <santo@dcariotti.me>
2025-04-22 09:48:26 +0200

    Add release.sh script

---
+++ release.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ $# -eq 0 ]; then
+    echo "You must pass the version number."
+    exit 1
+fi
+
+sed -i "15s/.*/LABEL version=\"$1\"/" docker/api/Dockerfile
+sed -i "17s/.*/LABEL version=\"$1\"/" docker/ui/Dockerfile
+git add docker
+git commit -m "release: version $1"
+git tag -a "v$1" -m "Version $1"
